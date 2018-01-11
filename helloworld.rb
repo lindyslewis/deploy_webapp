29becd86b50052ca796c5e8fa94c186a19a0564c
Lindy Nelson <lindynelson@lindys-air.attlocal.net>
2018-01-11 11:24:35 -0600

    Added Form

--- helloworld.rb
+++ helloworld.rb
@@ -3,7 +3,10 @@
 require 'sinatra'
 
 get '/' do
-    "Hello world!"
+    output = "Hello world!"
+    output << "<br/>"
+    output << "<a href=\"/goodbye\">Leave</a>"
+    output
 end
 
 get '/goodbye' do 
@@ -15,3 +18,16 @@ get '/about' do
     output << "<p>This is my first Sinatra app.</p>"
     output
 end
+
+get '/form' do 
+    form = '<form action="/form" method="post">'
+    form << '<input type="submit" value="Submit" />'
+    form << '</form>'
+    form 
+end
+
+post '/form' do
+     "Thank you for submitting the form."
+end
+
+
