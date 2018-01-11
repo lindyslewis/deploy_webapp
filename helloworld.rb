14d283b0b77060fe93d31a307780913b5dbf0fac
Lindy Nelson <lindynelson@lindys-air.attlocal.net>
2018-01-11 12:05:50 -0600

    Added Flexible Routes

--- helloworld.rb
+++ helloworld.rb
@@ -13,12 +13,6 @@ get '/goodbye' do
     "Goodbye world!"
 end
 
-get '/about' do
-    output = "<h2>Sinatra</h2>"
-    output << "<p>This is my first Sinatra app.</p>"
-    output
-end
-
 get '/form' do 
     form = '<form action="/form" method="post">'
     form << '<input type="submit" value="Submit" />'
@@ -30,4 +24,16 @@ post '/form' do
      "Thank you for submitting the form."
 end
 
+get '/company/about' do
+    output = "<h2>Sinatra</h2>"
+    output << "<p>This is my first Sinatra app.</p>"
+    output
+end
 
+#get /\A\/company\/[A-Za-z\-]+\Z/ do
+#    "This route matches a Regex."
+#end
+
+get '/company/*' do
+    "This route catches everything else."
+end
