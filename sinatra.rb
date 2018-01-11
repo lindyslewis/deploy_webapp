db8d10cb5b44a1e8461df3d3e4f4aaef373c257f
Lindy Nelson <lindynelson@lindys-air.attlocal.net>
2018-01-11 12:40:01 -0600

    Added redirects for root

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+# Default file paths
+# set :root, File.dirname(__FILE__)
+# set :public_folder, settings.root + "public"
+# set :views, settings.root + "views"
+
 get '/' do
-    "Explore California"
+    filepath = File.join(settings.public_folder, 'index.html')
+    send_file(filepath)
 end
