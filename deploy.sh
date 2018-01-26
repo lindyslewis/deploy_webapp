0b1c652c0ed9a201af0d336a216d36dc02489bcc
Lindy Lewis <lindyslewis@gmail.com>
2018-01-26 09:38:20 -0600

    Cloud Formation create stack added to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo 'hello from deploy.sh'
+
+stack_name="$1"
+echo $stack_name
+
+aws cloudformation create-stack --stack-name "$stack_name" --template-body file://cloud_formation_template.json
