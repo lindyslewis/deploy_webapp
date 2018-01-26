b750e66df9331baf92c1de1be8869ef0e766ee11
Lindy Lewis <lindyslewis@gmail.com>
2018-01-26 12:13:04 -0600

    Add region parameter to aws command

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ echo 'hello from deploy.sh'
 stack_name="$1"
 echo $stack_name
 
-aws cloudformation create-stack --stack-name "$stack_name" --template-body file://cloud_formation_template.json
+aws cloudformation create-stack --stack-name "$stack_name" --region "us-east-1"  --template-body file://cloud_formation_template.json
