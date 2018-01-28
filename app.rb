eaa1fe75cd475cf6cf819476b713d9dafa8f7cf6
Lindy Lewis <lindyslewis@gmail.com>
2018-01-27 18:14:03 -0600

    Added instance ID to html

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 #app.ru
 require 'sinatra'
+require 'net/http'
 
 set :bind, '0.0.0.0'
 
+url =  'http://169.254.169.254/latest/meta-data/instance-id' 
+
+uri = URI(url)
+output = Net::HTTP.get(uri) # => String
+
 get '/' do 
-  "hello world!"
+  "hello world from instance number: #{output} "
 end
